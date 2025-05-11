64fc8cef68ca42868462cb588cc834ccec59d961
Arda Fırat <ardafirat375@gmail.com>
2025-05-11 22:31:00 +0300

    Add files via upload

---
+++ TuneInMeProject/tuneinmeapp/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from .models import Movie, WatchedMovie
+
+admin.site.register(Movie)
+admin.site.register(WatchedMovie)
+from django.contrib import admin
+
+# Register your models here.
---
+++ TuneInMeProject/tuneinmeapp/forms.py
@@ -0,0 +1,8 @@
+from django import forms
+from .models import WatchedMovie
+
+
+class WatchedMovieForm(forms.ModelForm):
+    class Meta:
+        model = WatchedMovie
+        fields = ['movie', 'rating', 'review']
