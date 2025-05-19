a50a8f8fd19bcd00f5a5aecbccfdcc2dad350cf3
Arda Fırat <ardafirat375@gmail.com>
2025-05-19 23:21:34 +0300

    Delete TuneInMeProject/TuneInMeProject directory

--- TuneInMeProject/TuneInMeProject/tuneinmeapp/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class TuneinmeappConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'tuneinmeapp'
--- TuneInMeProject/TuneInMeProject/tuneinmeapp/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.home, name='home'),
-    path('watched-movies/', views.watched_movies, name='watched_movies'),
-    path('profile/', views.profile, name='profile'),
-    path('chatbot/', views.chatbot, name='chatbot'),
-]
