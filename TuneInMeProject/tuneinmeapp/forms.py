4c73bb6577e0ebc52bca34cb02854710c688af6a
Arda Fırat <ardafirat375@gmail.com>
2025-05-19 16:37:34 +0300

    Delete TuneInMeProject directory

--- TuneInMeProject/tuneinmeapp/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-from .models import WatchedMovie
-
-
-class WatchedMovieForm(forms.ModelForm):
-    class Meta:
-        model = WatchedMovie
-        fields = ['movie', 'rating', 'review']
